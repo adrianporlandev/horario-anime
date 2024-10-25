7b4f96d91bf816fe984b06761517528390d45a34
Adrian <adriporlanlopez11@gmail.com>
2024-10-25 15:00:22 +0200

    Refactor tailwind configuration and add Astro integration
    - Refactor tailwind.config.mjs to extend the theme
    - Add Astro integration in astro.config.mjs
    - Update package.json dependencies for Astro and tailwindcss
    - Add horario.js data file for anime schedule
    - Update Layout.astro to include title and description props
    - Add HorarioSemanl.astro component for displaying weekly anime schedule
    - Update index.astro to use HorarioSemanl component

---
+++ src/data/horario.js
@@ -0,0 +1,16 @@
+// /src/data/horario.js
+export const horario = [
+    { dia: "Lunes", nombre: "Ao no Hako", hora: "16:00", tipo:"Manga" },
+    { dia: "Miercoles", nombre: "Dan Da Dan", hora: "18:00", tipo:"Anime" },
+    { dia: "Miercoles", nombre: "Rent a Girlfriend", hora: "16:00", tipo:"Manga" },
+    { dia: "Jueves", nombre: "Ao no Hako", hora: "00:00", tipo:"Anime" },
+    { dia: "Jueves", nombre: "One Piece", hora: "18:00", tipo:"Manga" },
+    { dia: "Viernes", nombre: "Dragon Ball Daima", hora: "19:00", tipo:"Anime" },
+    { dia: "Sabado", nombre: "Blue Lock", hora: "18:00", tipo:"Anime" },
+    { dia: "Sabado", nombre: "Boku no Hero Academia", hora: "11:40", tipo:"Anime" },
+    { dia: "Domingo", nombre: "One Piece", hora: "10:00", tipo:"Anime" },
+    { dia: "Domingo", nombre: "Uzumaki", hora: "15:00", tipo:"Anime" },
+    { dia: "Domingo", nombre: "MF Ghost", hora: "18:00", tipo:"Anime" },
+
+  ];
+  
