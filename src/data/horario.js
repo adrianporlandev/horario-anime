732f9fe67d5d73856f69569a1bf327da960df94c
Adrian Porlan <adriporlanlopez11@gmail.com>
2025-01-08 16:25:36 +0100

    update seasonal animes

--- src/data/horario.js
+++ src/data/horario.js
@@ -1,15 +1,17 @@
 // /src/data/horario.js
 export const horario = [
     { dia: "Domingo", nombre: "Ao no Hako", hora: "16:00", tipo:"Manga" },
-    { dia: "Jueves", nombre: "Dan Da Dan", hora: "16:00", tipo:"Anime" },
     { dia: "Miercoles", nombre: "Rent a Girlfriend", hora: "16:00", tipo:"Manga" },
     { dia: "Jueves", nombre: "Ao no Hako", hora: "00:00", tipo:"Anime" },
     { dia: "Jueves", nombre: "One Piece", hora: "18:00", tipo:"Manga" },
-    { dia: "Viernes", nombre: "Dragon Ball Daima", hora: "19:00", tipo:"Anime" },
-    { dia: "Sabado", nombre: "Blue Lock", hora: "18:00", tipo:"Anime" },
     { dia: "Domingo", nombre: "One Piece", hora: "10:00", tipo:"Anime" },
-    { dia: "Domingo", nombre: "Uzumaki", hora: "15:00", tipo:"Anime" },
-    { dia: "Domingo", nombre: "MF Ghost", hora: "18:00", tipo:"Anime" },
+    { dia: "Lunes", nombre: "Medaka Kuroiwa Is Impervious to My Charms", hora: "20:00", tipo:"Anime" },
+    { dia: "Lunes", nombre: "I Have a Crush at Work", hora: "18:00", tipo:"Anime" },
+    { dia: "Sabado", nombre: "Solo Leveling", hora: "18:00", tipo:"Anime" },
+    { dia: "Sabado", nombre: "Sakamoto Days", hora: "18:00", tipo:"Anime" },
+    { dia: "Jueves", nombre: "Dr Stone", hora: "18:00", tipo:"Anime" },
+    { dia: "Miercoles", nombre: "Ameku Takao's Detective Karte", hora: "8:00", tipo:"Anime" },
+
 
   ];
   
