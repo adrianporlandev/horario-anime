eeca4aae8ed07340ad926e1b3970fbcfbc45668d
Adrian Porlan <86915360+adrianporlandev@users.noreply.github.com>
2025-04-11 02:45:19 +0000

    Update animes to actual season

--- src/data/horario.js
+++ src/data/horario.js
@@ -2,17 +2,19 @@
 export const horario = [
     { dia: "Domingo", nombre: "Ao no Hako", hora: "16:00", tipo:"Manga" },
     { dia: "Miercoles", nombre: "Rent a Girlfriend", hora: "16:00", tipo:"Manga" },
-    { dia: "Jueves", nombre: "Ao no Hako", hora: "00:00", tipo:"Anime" },
     { dia: "Jueves", nombre: "One Piece", hora: "18:00", tipo:"Manga" },
-    { dia: "Domingo", nombre: "One Piece", hora: "10:00", tipo:"Anime" },
-    { dia: "Lunes", nombre: "Medaka Kuroiwa Is Impervious to My Charms", hora: "20:00", tipo:"Anime" },
-    { dia: "Lunes", nombre: "I Have a Crush at Work", hora: "18:00", tipo:"Anime" },
-    { dia: "Sabado", nombre: "Solo Leveling", hora: "18:00", tipo:"Anime" },
-    { dia: "Sabado", nombre: "Sakamoto Days", hora: "18:00", tipo:"Anime" },
-    { dia: "Jueves", nombre: "Dr Stone", hora: "18:00", tipo:"Anime" },
-    { dia: "Miercoles", nombre: "Ameku Takao's Detective Karte", hora: "8:00", tipo:"Anime" },
     { dia: "Domingo", nombre: "Blue Lock", hora: "16:00", tipo: "Manga"},
-    { dia: "Domingo", nombre: "100 Girlfriends", hora: "16:05", tipo: "Anime"},
+    { dia: "Domingo", nombre: "One Piece", hora: "10:00", tipo:"Anime" },
+    { dia: "Jueves", nombre: "Wind Breaker", hora: "20:30", tipo: "Anime" },
+    { dia: "Viernes", nombre: "Danjo no Yuujou wa Seiritsu suru?", hora: "16:00", tipo: "Anime"},
+    { dia: "Sabado", nombre: "To Be Hero X", hora: "2:30", tipo: "Anime" },
+    { dia: "Domingo", nombre: "Witch Watch", hora: "10:30", tipo: "Anime" },
+    { dia: "Domingo", nombre: "Lazarus", hora: "15:00", tipo: "Anime" },
+    { dia: "Domingo", nombre: "Sentai Dai", hora: "15:00", tipo: "Anime" },
+
+
+
+
 
 
 
