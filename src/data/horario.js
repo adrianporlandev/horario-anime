7ed5d75740519337d15cd0fb6aa17608a0636c58
Adrian Porlan <86915360+adrianporlandev@users.noreply.github.com>
2025-04-29 16:17:39 +0200

    agregadas nuevas series

--- src/data/horario.js
+++ src/data/horario.js
@@ -1,17 +1,19 @@
 // /src/data/horario.js
 export const horario = [
-    { dia: "Domingo", nombre: "Ao no Hako", hora: "16:00", tipo:"Manga" },
+    { dia: "Martes", nombre: "Shiunji-ke no Kodomotachi", hora:"16:00", tipo:"anime"},
+    { dia: "Lunes", nombre: "The Last of Us", hora:"15:00", tipo:"serie"},
     { dia: "Miercoles", nombre: "Rent a Girlfriend", hora: "16:00", tipo:"Manga" },
     { dia: "Jueves", nombre: "One Piece", hora: "18:00", tipo:"Manga" },
-    { dia: "Domingo", nombre: "Blue Lock", hora: "16:00", tipo: "Manga"},
-    { dia: "Domingo", nombre: "One Piece", hora: "10:00", tipo:"Anime" },
     { dia: "Jueves", nombre: "Wind Breaker", hora: "20:30", tipo: "Anime" },
     { dia: "Viernes", nombre: "Danjo no Yuujou wa Seiritsu suru?", hora: "16:00", tipo: "Anime"},
     { dia: "Sabado", nombre: "To Be Hero X", hora: "2:30", tipo: "Anime" },
+    { dia: "Domingo", nombre: "Blue Lock", hora: "16:00", tipo: "Manga"},
+    { dia: "Domingo", nombre: "One Piece", hora: "10:00", tipo:"Anime" },
     { dia: "Domingo", nombre: "Witch Watch", hora: "10:30", tipo: "Anime" },
     { dia: "Domingo", nombre: "Lazarus", hora: "15:00", tipo: "Anime" },
     { dia: "Domingo", nombre: "Sentai Dai", hora: "15:00", tipo: "Anime" },
-
+    { dia: "Domingo", nombre: "Ao no Hako", hora: "16:00", tipo:"Manga" },
+    
 
 
 
