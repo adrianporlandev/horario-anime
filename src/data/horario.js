b57934613ad846fb6fe941a2f08094a52f6cf35b
Adrian Porlan <86915360+adrianporlandev@users.noreply.github.com>
2025-05-24 01:34:09 +0200

    order by day and update anime day

--- src/data/horario.js
+++ src/data/horario.js
@@ -6,7 +6,7 @@ export const horario = [
     { dia: "Jueves", nombre: "One Piece", hora: "18:00", tipo:"Manga" },
     { dia: "Jueves", nombre: "Wind Breaker", hora: "20:30", tipo: "Anime" },
     { dia: "Viernes", nombre: "Danjo no Yuujou wa Seiritsu suru?", hora: "16:00", tipo: "Anime"},
-    { dia: "Sabado", nombre: "To Be Hero X", hora: "2:30", tipo: "Anime" },
+    { dia: "Domingo", nombre: "To Be Hero X", hora: "2:30", tipo: "Anime" },
     { dia: "Domingo", nombre: "Blue Lock", hora: "16:00", tipo: "Manga"},
     { dia: "Domingo", nombre: "One Piece", hora: "10:00", tipo:"Anime" },
     { dia: "Domingo", nombre: "Witch Watch", hora: "10:30", tipo: "Anime" },
