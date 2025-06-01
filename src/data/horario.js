e37a56dfd7447b8c847d3b12ee20242b2de822a4
Adrian Porlan <86915360+adrianporlandev@users.noreply.github.com>
2025-06-01 22:53:46 +0200

    update data

--- src/data/horario.js
+++ src/data/horario.js
@@ -1,13 +1,12 @@
 // /src/data/horario.js
 export const horario = [
-    { dia: "Lunes", nombre: "The Last of Us", hora:"15:00", tipo:"Serie"},
     { dia: "Martes", nombre: "Shiunji-ke no Kodomotachi", hora:"16:00", tipo:"Anime"},
     { dia: "Miercoles", nombre: "Rent a Girlfriend", hora: "16:00", tipo:"Manga" },
     { dia: "Jueves", nombre: "One Piece", hora: "18:00", tipo:"Manga" },
     { dia: "Jueves", nombre: "Wind Breaker", hora: "20:30", tipo: "Anime" },
     { dia: "Viernes", nombre: "Danjo no Yuujou wa Seiritsu suru?", hora: "16:00", tipo: "Anime"},
-    { dia: "Domingo", nombre: "To Be Hero X", hora: "2:30", tipo: "Anime" },
-    { dia: "Domingo", nombre: "Blue Lock", hora: "16:00", tipo: "Manga"},
+    { dia: "Domingo", nombre: "To Be Hero X", hora: "02:30", tipo: "Anime" },
+    { dia: "Martes", nombre: "Blue Lock", hora: "16:00", tipo: "Manga"},
     { dia: "Domingo", nombre: "One Piece", hora: "10:00", tipo:"Anime" },
     { dia: "Domingo", nombre: "Witch Watch", hora: "10:30", tipo: "Anime" },
     { dia: "Domingo", nombre: "Lazarus", hora: "15:00", tipo: "Anime" },
