232fba7495e661888f23590a5740a6081eeee2cf
adrianporlandev <adriporlanlopez11@gmail.com>
2025-06-30 19:28:24 +0200

    add new anime and fix other

--- src/data/horario.js
+++ src/data/horario.js
@@ -1,5 +1,6 @@
 // /src/data/horario.js
 export const horario = [
+    { dia:"Lunes", nombre: "One Piece", hora: "18:00", tipo:"Anime" },
     { dia:"Martes", nombre:"Sakamoto Days", hora:"16:00", tipo:"Anime", fecha:"15 Julio"},
     { dia:"Martes", nombre: "Blue Lock", hora: "16:00", tipo: "Manga"},
     { dia:"Martes", nombre:"Grand Blue", hora:"16:00", tipo:"Anime", fecha:"8 Julio"},
@@ -9,10 +10,10 @@ export const horario = [
     { dia:"Jueves", nombre:"Rent a Girlfriend", hora:"16:00", tipo:"Anime", fecha:"5 Julio"},
     { dia:"Viernes", nombre:"Dandadan", hora:"16:00", tipo:"Anime", fecha:"4 Julio"},
     { dia:"Viernes", nombre:"Yofukashi no Uta", hora:"16:00", tipo:"Anime", fecha:"4 Julio"},
+    { dia:"Viernes", nombre:"Takopii no Genzou", hora:"17:00", tipo:"Anime"},
     { dia:"Sabado", nombre:"Seishun Buta Yarou wa Santa Claus no Yume wo Minai", hora:"16:00", tipo:"Anime", fecha:"5 Julio"},
     { dia:"Sabado", nombre:"Kaijuu 8", hora:"16:00", tipo:"Anime", fecha:"19 Julio"},
     { dia:"Domingo", nombre: "To Be Hero X", hora: "02:30", tipo: "Anime" },
-    { dia:"Domingo", nombre: "One Piece", hora: "10:00", tipo:"Anime" },
     { dia:"Domingo", nombre: "Witch Watch", hora: "10:30", tipo: "Anime" },
     { dia:"Domingo", nombre: "Ao no Hako", hora: "16:00", tipo:"Manga" },
     { dia:"Domingo", nombre:"Sono Bique Doll", hora:"16:00", tipo:"Anime", fecha:"6 Julio"},
