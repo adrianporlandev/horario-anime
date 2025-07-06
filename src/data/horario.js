707bf0933a919ef61d089415f75c9b4ab5518597
adrianporlandev <adriporlanlopez11@gmail.com>
2025-07-06 22:21:22 +0200

    update anime schedule with new entries and correct timings

--- src/data/horario.js
+++ src/data/horario.js
@@ -1,23 +1,23 @@
 // /src/data/horario.js
 export const horario = [
-    { dia:"Lunes", nombre: "One Piece", hora: "18:00", tipo:"Anime" },
+    { dia:"Lunes", nombre:"Grand Blue", hora:"16:00", tipo:"Anime", fecha:"7 Julio"},
     { dia:"Martes", nombre:"Sakamoto Days", hora:"16:00", tipo:"Anime", fecha:"15 Julio"},
     { dia:"Martes", nombre: "Blue Lock", hora: "16:00", tipo: "Manga"},
-    { dia:"Martes", nombre:"Grand Blue", hora:"16:00", tipo:"Anime", fecha:"8 Julio"},
+    { dia:"Martes", nombre:"Rent a Girlfriend", hora:"16:00", tipo:"Anime"},
+    { dia:"Miercoles", nombre: "One Piece", hora: "18:00", tipo:"Anime" },
     { dia:"Miercoles", nombre: "Rent a Girlfriend", hora: "16:00", tipo:"Manga" },
     { dia:"Jueves", nombre: "One Piece", hora: "18:00", tipo:"Manga" },
     { dia:"Jueves", nombre:"Dr Stone", hora:"16:00", tipo:"Anime", fecha:"10 Julio"},
-    { dia:"Jueves", nombre:"Rent a Girlfriend", hora:"16:00", tipo:"Anime", fecha:"5 Julio"},
-    { dia:"Viernes", nombre:"Dandadan", hora:"16:00", tipo:"Anime", fecha:"4 Julio"},
-    { dia:"Viernes", nombre:"Yofukashi no Uta", hora:"16:00", tipo:"Anime", fecha:"4 Julio"},
+    { dia:"Jueves", nombre:"Dandadan", hora:"18:00", tipo:"Anime"},
+    { dia:"Viernes", nombre:"Yofukashi no Uta", hora:"16:00", tipo:"Anime"},
     { dia:"Viernes", nombre:"Takopii no Genzou", hora:"17:00", tipo:"Anime"},
-    { dia:"Sabado", nombre:"Seishun Buta Yarou wa Santa Claus no Yume wo Minai", hora:"16:00", tipo:"Anime", fecha:"5 Julio"},
-    { dia:"Sabado", nombre:"Kaijuu 8", hora:"16:00", tipo:"Anime", fecha:"19 Julio"},
+    { dia:"Sabado", nombre:"Seishun Buta Yarou wa Santa Claus no Yume wo Minai", hora:"18:00", tipo:"Anime"},
+    { dia:"Sabado", nombre:"Kaijuu 8", hora:"16:40", tipo:"Anime"},
+    { dia:"Sabado", nombre:"Sono Bique Doll", hora:"18:00", tipo:"Anime"},
     { dia:"Domingo", nombre: "To Be Hero X", hora: "02:30", tipo: "Anime" },
     { dia:"Domingo", nombre: "Witch Watch", hora: "10:30", tipo: "Anime" },
     { dia:"Domingo", nombre: "Ao no Hako", hora: "16:00", tipo:"Manga" },
-    { dia:"Domingo", nombre:"Sono Bique Doll", hora:"16:00", tipo:"Anime", fecha:"6 Julio"},
-    { dia:"Domingo", nombre:"Kaoru Hana wa Rin to Saku", hora:"16:00", tipo:"Anime", fecha:"6 Julio"},
+    { dia:"Domingo", nombre:"Kaoru Hana wa Rin to Saku", hora:"23:00", tipo:"Anime", fecha:"13 Julio"},
    
    
 
