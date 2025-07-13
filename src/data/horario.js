96a317a48465c6617fafc2c9102f10838b49c2b9
adrianporlandev <adriporlanlopez11@gmail.com>
2025-07-13 18:12:05 +0200

    Fixed schedule

--- src/data/horario.js
+++ src/data/horario.js
@@ -7,17 +7,18 @@ export const horario = [
     { dia:"Miercoles", nombre: "One Piece", hora: "18:00", tipo:"Anime" },
     { dia:"Miercoles", nombre: "Rent a Girlfriend", hora: "16:00", tipo:"Manga" },
     { dia:"Jueves", nombre: "One Piece", hora: "18:00", tipo:"Manga" },
-    { dia:"Jueves", nombre:"Dr Stone", hora:"16:00", tipo:"Anime", fecha:"10 Julio"},
+    { dia:"Jueves", nombre:"Dr Stone", hora:"16:00", tipo:"Anime"},
     { dia:"Jueves", nombre:"Dandadan", hora:"18:00", tipo:"Anime"},
-    { dia:"Viernes", nombre:"Yofukashi no Uta", hora:"16:00", tipo:"Anime"},
+    { dia:"Viernes", nombre:"Yofukashi no Uta", hora:"16:30", tipo:"Anime"},
     { dia:"Viernes", nombre:"Takopii no Genzou", hora:"17:00", tipo:"Anime"},
     { dia:"Sabado", nombre:"Seishun Buta Yarou wa Santa Claus no Yume wo Minai", hora:"18:00", tipo:"Anime"},
     { dia:"Sabado", nombre:"Kaijuu 8", hora:"16:40", tipo:"Anime"},
     { dia:"Sabado", nombre:"Sono Bique Doll", hora:"18:00", tipo:"Anime"},
+    { dia:"Sabado", nombre:"Kaoru Hana wa Rin to Saku", hora:"17:00", tipo:"Anime"},
     { dia:"Domingo", nombre: "To Be Hero X", hora: "02:30", tipo: "Anime" },
     { dia:"Domingo", nombre: "Witch Watch", hora: "10:30", tipo: "Anime" },
     { dia:"Domingo", nombre: "Ao no Hako", hora: "16:00", tipo:"Manga" },
-    { dia:"Domingo", nombre:"Kaoru Hana wa Rin to Saku", hora:"23:00", tipo:"Anime", fecha:"13 Julio"},
+    { dia:"Domingo", nombre:"Gachiakuta", hora:"17:00", tipo:"Anime" },
    
    
 
