0444f25e889cdd35e1bf10a99f9c911b433648bf
Adrian Porlan <86915360+adrianporlandev@users.noreply.github.com>
2026-01-08 23:40:09 +0100

    update winter season

--- src/data/horario.js
+++ src/data/horario.js
@@ -5,11 +5,12 @@ export const horario = [
     { dia:"Miercoles", nombre: "Rent a Girlfriend", hora: "16:00", tipo:"Manga" },
     { dia:"Miercoles", nombre: "FallOut", hora: "16:00", tipo:"Serie" },
     { dia:"Jueves", nombre: "One Piece", hora: "18:00", tipo:"Manga" },
-    { dia:"Viernes", nombre: "Sanda", hora: "20:00", tipo:"Anime" },
-    { dia:"Sabado", nombre: "Boku no Hero", hora: "21:00", tipo:"Anime" },
-    { dia:"Domingo", nombre: "Ao no Hako", hora: "16:00", tipo:"Manga" },
-    { dia:"Domingo", nombre: "One Punch Man", hora: "18:00", tipo:"Anime" },
-    { dia:"Domingo", nombre: "Tomodachi no Imouto", hora: "18:00", tipo:"Anime" },
+    { dia:"Jueves", nombre:"Jujutsu Kaisen", hora:"20:00", tipo:"Anime"},
+    { dia:"Jueves", nombre:"Mato Seihei no Slave", hora:"17:00", tipo:"Anime"},
+    { dia:"Jueves", nombre:"MF Ghost", hora:"15:00", tipo:"Anime"},
+    { dia:"Viernes", nombre:"Sousou no Frieren", hora:"18:00", tipo:"Anime"},
+    { dia:"Miercoles", nombre:"Oshi no Ko", hora:"18:00", tipo:"Anime"},
+
 
 
    
