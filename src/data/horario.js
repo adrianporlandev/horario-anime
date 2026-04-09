655bf881df9bf4f7762077bc0978159730444744
Adrian Porlan <adriporlanlopez11@gmail.com>
2026-04-09 22:39:40 +0200

    Actualizar horario y mejorar la gestión de eventos mensuales

--- src/data/horario.js
+++ src/data/horario.js
@@ -1,11 +1,16 @@
 // /src/data/horario.js
+// Si agregas `url` en un evento, la tarjeta se vuelve clicable en el horario.
+// Para eventos mensuales usa: frecuencia: "mensual" y fecha: "15 de cada mes".
 export const horario = [
  
     //Manga
-    { dia:"Martes", nombre: "Blue Lock", hora: "16:00", tipo: "Manga"},
-    { dia:"Miercoles", nombre: "Rent a Girlfriend", hora: "16:00", tipo:"Manga" },
-    { dia:"Jueves", nombre: "One Piece", hora: "18:00", tipo:"Manga" },
-    { dia:"Domingo", nombre:"Blue Box", hora:"16:00", tipo:"Manga"},
+    { dia:"Martes", nombre: "Blue Lock", hora: "16:00", tipo: "Manga", url: "https://capibaratraductor.com/senshimanga/manga/blue-lock" },
+    { dia:"Miercoles", nombre: "Rent a Girlfriend", hora: "16:00", tipo:"Manga", url: "https://www.spnmanga.com/manga/l8w82q/kanojo-okarishimasu/" },
+    { dia:"Jueves", nombre: "One Piece", hora: "18:00", tipo:"Manga", url: "https://fokascans.pages.dev/manga/onepiece" },
+    { dia:"Domingo", nombre:"Blue Box", hora:"16:00", tipo:"Manga", url: "https://mangaplus.shueisha.co.jp/titles/200083" },
+    { nombre:"Atras del Supermercado", hora:"18:00", tipo:"Manga",frecuencia: "mensual", fecha: "9 de cada mes", url: "https://www.spnmanga.com/manga/77tul3/hostorias-que-ocurren-al-fumar-en-la-parte-trasera-del-supermercado/" },
+    { nombre:"Zom 100", hora:"18:00", tipo:"Manga",frecuencia: "mensual", fecha: "9 de cada mes", url: "https://www.spnmanga.com/manga/55843j/zombie-100-100-things-i-want-to-do-before-i-become-a-zombie/" },
+
     //Animes de Temporada
     {dia:"Miercoles", nombre:"Classroom of the Elite ", hora:"14:30", tipo:"Anime"},
     {dia:"Jueves", nombre:"Koori no Jouheki", hora:"18:00", tipo:"Anime"},
