4871f9ba1b1245e128e649cbff41e963522c0c4b
Adrian Porlan <adriporlanlopez11@gmail.com>
2026-07-07 22:56:32 +0200

    fix day

--- src/data/horario.js
+++ src/data/horario.js
@@ -12,16 +12,12 @@ export const horario = [
   { nombre: "Zom 100", hora: "18:00", tipo: "Manga", frecuencia: "mensual", fecha: "9 de cada mes", url: "https://www.spnmanga.com/manga/55843j/zombie-100-100-things-i-want-to-do-before-i-become-a-zombie/" },
 
   //Animes de Temporada
-  {
-    dia: "Jueves", nombre: "Atras del Supermercado", hora: "18:00", tipo: "Anime", url: "https://jellyfin.local.adrianporlan.com/web/#/details?id=853b1e771923b66d3b6810052d0740e8&serverId=30a3fa2f6d284f5eab7f3174571e5d99"
-  },
-  { dia: "Martes", nombre: "Grand Blue", hora: "18:00", tipo: "Anime", url: "https://jellyfin.local.adrianporlan.com/web/#/details?id=3da6b06e446730891b84b0d41a325718&serverId=30a3fa2f6d284f5eab7f3174571e5d99" },
+  { dia: "Lunes", nombre: "Grand Blue", hora: "18:00", tipo: "Anime", url: "https://jellyfin.local.adrianporlan.com/web/#/details?id=3da6b06e446730891b84b0d41a325718&serverId=30a3fa2f6d284f5eab7f3174571e5d99" },
+  { dia: "Jueves", nombre: "Atras del Supermercado", hora: "18:00", tipo: "Anime", url: "https://jellyfin.local.adrianporlan.com/web/#/details?id=853b1e771923b66d3b6810052d0740e8&serverId=30a3fa2f6d284f5eab7f3174571e5d99" },
   { dia: "Domingo", nombre: "You and I Are Polar Opposites", hora: "18:00", tipo: "Anime", url: "https://jellyfin.local.adrianporlan.com/web/#/details?id=b1da7a3a6acd740f7fefe2616012a00f&serverId=30a3fa2f6d284f5eab7f3174571e5d99" },
   { dia: "Domingo", nombre: "100 Novias", hora: "18:00", tipo: "Anime", url: "https://jellyfin.local.adrianporlan.com/web/#/details?id=93cf253a829db4c4b53c0e558d6f1e97&serverId=30a3fa2f6d284f5eab7f3174571e5d99" },
 
-
-
-  //Series          
+  //Series
   { dia: "Lunes", nombre: "Casa del Dragon", hora: "12:00", tipo: "Serie", url: "https://series.ly/series/la-casa-del-dragon" }
 
 
